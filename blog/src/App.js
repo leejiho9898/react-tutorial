f9176af98e5888eeb06ae04be3a2ccd499760726
이지호 <cctv1998@naver.com>
2021-06-06 22:33:05 +0900

    modify

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let [글제목,글제목변경] = useState('남자 코트 추천');
   let posts = "경남 고기 맛집";
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={{color:'blue',fontSize:'30px'}}>개발 blog</div>
+        <div>개발 blog</div>
       </div>
-      <img src={logo} />
-      <h4>{ posts }</h4>
+      <div className="list">
+        <h3> {글제목} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+        <h3> {글제목} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+        <h3> {글제목} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div> 
     </div>
   );
 }
