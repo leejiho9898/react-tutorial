ea447dd133f1c0c30b0b6a1d66bd0bd13cb2df55
이지호 <cctv1998@naver.com>
2021-06-09 15:19:44 +0900

    블로그 비스무리한게 만들어짐

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,31 +5,41 @@ function App() {
   let [글제목, 글제목변경] = useState([
     "남자 코트 추천",
     "조던1 컬러추천",
-    "덩크로우1 코디",
+    "덩크로우1 코디"
   ]);
   let [따봉, 따봉변경] = useState(0,0,0);
   let [modal, modal변경] = useState(false);
   let [누른제목,누른제목변경] = useState(0);
+  let [입력값,입력값변경] = useState('');
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
       {
-        글제목.map(function(a,i){
+        글제목.map(function(a,index){
           return( 
-          <div className="list">
-          <h3 onClick={()=>{누른제목변경(i)}}> {a} <span onClick={() => 따봉변경(따봉 + 1)} >👍</span>{따봉}</h3>
+          <div className="list" key={index}>
+          <h3 onClick={()=>{누른제목변경(index)}}> {a} <span onClick={() => 따봉변경(따봉 + 1)} >👍</span>{따봉}</h3>
           <p>2월 19일 발행</p>
           <hr />
           </div>
           )
         })
       }
+        <div className="publish">
+          <input onChange={ (e)=>{ 입력값변경(e.target.value) }} />
+          <button onClick={()=>{
+            var arrayCopy =[...글제목];
+            arrayCopy.unshift(입력값);
+            글제목변경(arrayCopy);
+          }}>저장</button>
+        </div>
 
-        <button onClick={ ()=> modal변경(!modal)}>버튼</button>
+
+        <button onClick={ ()=> modal변경(!modal)}>열고닫기</button>
         {modal === true ? <Modal 글제묵={글제목} 누른제묵={누른제목}/> : null}
-      </div>
+    </div>
   );
 }
 
